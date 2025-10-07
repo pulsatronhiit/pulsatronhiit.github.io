3ef48deb1a149e423457608202beb836111958fc
Tim de Putter <tim.deputter@buenting.de>
2025-10-07 22:52:08 +0200

    Remove service worker registration and related files for offline functionality

--- src/main.jsx
+++ src/main.jsx
@@ -2,13 +2,9 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import registerServiceWorker from './utils/serviceWorker.js'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
 )
-
-// Register service worker for offline functionality
-registerServiceWorker();
